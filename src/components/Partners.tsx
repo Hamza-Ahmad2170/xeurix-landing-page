55bf1034b5d7e9e5313b2160dded4ad287548a3d
Hamza Ahmad <106329588+Hamza-Ahmad2170@users.noreply.github.com>
2024-08-14 16:02:34 +0500

    changed image and added image
    changed hire smatter image and added partners image

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -4,11 +4,42 @@ import Paragraph from "./Paragraph";
 import partner1 from "../../public/images/partners/partner1_.jpeg";
 import Image from "next/image";
 import SectionHeader from "./SectionHeader";
+import xeurix from "../../public/images/partners/xeurix website pics icons.png";
+import icims from "../../public/images/partners/icims.png";
+import jazzhr from "../../public/images/partners/jazzhr.png";
+import greenHouse from "../../public/images/partners/greenhouse.png";
+import lever from "../../public/images/partners/lever_logo.png";
+import breezy from "../../public/images/partners/BreezyHR3.png";
+import taleo from "../../public/images/partners/taleo.png";
 
 const partners = [
   {
-    image: partner1,
-    alt: "Partner 1",
+    image: xeurix,
+    alt: "as",
+  },
+  {
+    image: icims,
+    alt: "asd",
+  },
+  {
+    image: jazzhr,
+    alt: "sd",
+  },
+  {
+    image: greenHouse,
+    alt: "da",
+  },
+  {
+    image: lever,
+    alt: "Lever",
+  },
+  {
+    image: breezy,
+    alt: "Breezy",
+  },
+  {
+    image: taleo,
+    alt: "Taleo",
   },
 ];
 
@@ -24,7 +55,7 @@ export default function Partners() {
               seamless workflows
             </Paragraph>
           </SectionHeader>
-          <div className="flex flex-wrap justify-center gap-8 py-10">
+          <div className="flex flex-wrap justify-center gap-8 py-10 items-center">
             {partners.map((partner) => (
               <div key={partner.alt} className="basis-24">
                 <Image src={partner.image} alt="" />
